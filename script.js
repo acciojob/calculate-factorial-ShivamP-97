047900261692b13ebf5894166f7a280cc88570ac
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-28 18:34:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let num = parseInt(prompt("Enter a number:"));
+
+function factorial(n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+let fact = factorial(num);
+
+alert("The factorial of " + num + " is " + fact);
